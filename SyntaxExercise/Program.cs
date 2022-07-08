d5e9f864d36bef9ea5b2d2c0ede09b6615667d60
Hyun Kong <konghyunsik7@gmail.com>
2022-07-08 10:34:51 -0400

    inital

---
+++ SyntaxExercise/Program.cs
@@ -0,0 +1,41 @@
+﻿
+//Given the following code, change the code to implement:
+//inferred typing
+//string interpolation
+//and the ternary operator
+//int answer = 4;
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+
+
+//   response = answer + " is greater than or equal to nine";
+//string response;
+//if (answer < 9)
+//{
+//    response = answer + "is less than nine";
+//}
+//else
+//{
+//    response = answer + "greater than or equal to nine";
+//}
+
+
+int answer = 4;
+string isAnswer = (answer < 9) ? "is less than nine" : "greater than or equal to nine";
+
+Console.WriteLine($"{answer} {isAnswer}");
+               
+
+
+
+
+
+
+
+
+
